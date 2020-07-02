ddc7ada716001a7df5c71a536bd3db7bc92d3294
Huskie <huskielunar@gmail.com>
2020-07-02 23:48:48 +0200

    Automatic deploy

--- web_app/app/home/views.py
+++ web_app/app/home/views.py
@@ -1,13 +1,23 @@
 from flask import render_template
 from . import home
-
+import os
+import subprocess
 
 @home.route('/')
 def homepage():
     """
     Render the homepage template on the / route
     """
-    return render_template('page/home/index.html', title="Welcome")
+    #os.system('apt-cache search crypto')
+    out = subprocess.Popen(['apt-cache', 'search', 'crypto'], 
+           stdout=subprocess.PIPE, 
+           stderr=subprocess.STDOUT)
+    stdout,stderr = out.communicate()
+    print(stdout)
+    col=len(str(stdout).replace('\\n', '\n').split(str("\n")))
+    print(col)
+    crypto=str(stdout).replace('\\n', '\n').split(str("\n"))
+    return render_template('page/home/index.html',crypto=crypto,col=col, title="Home Page")
 
 
 @home.route('/dashboard')
