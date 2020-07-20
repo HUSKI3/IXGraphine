5649549777b0b4e094061872988cff065bcd808f
HUSKI3 <huskielunar@gmail.com>
2020-07-20 16:28:40 +0200

    Automatic deploy

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 
 echo "Installing IXGraphine"
-cd dev &&
-pip3 install -r requirements.txt &&
-npm install . 
+#cd dev 
+#pip3 install -r requirements.txt &&
+#npm install . 
 mkdir /etc/IXGraphite
-cp dev /etc/IXGraphite
+cp -P dev /etc/IXGraphite
 echo "Creating shortcut"
 chmod u+x /etc/IXGraphite/IXGrapite.desktop
-mv /dev/IXGraphite.desktop ~/Desktop
+cp -P dev/IXGraphite.desktop ~/Desktop
